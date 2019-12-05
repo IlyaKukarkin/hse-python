c3270ad1f3986d98b90a9a2779c9768517b0cea4
iakukarkin <ilya1196@yandex.ru>
2019-12-05 20:37:29 +0500

    add Health routes and method

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from models import Footballer, db
+from models import Footballer, db, Health
 
 index = Blueprint('index', __name__, url_prefix='/')
 api = Blueprint('api', __name__, url_prefix='/api')
@@ -28,7 +28,7 @@ def get_players():
 @api.route('/player/<int:id>')
 def get_player(id):
     player = Footballer.query.get(id)
-    return player.json() if player else 'Not found'
+    return player.json() if player else 'Player not found'
 
 
 @api.route('/player/add')
@@ -36,7 +36,25 @@ def put_player():
     name = request.args.get('name')
     surname = request.args.get('surname')
     club = request.args.get('club')
-    player = Footballer(name=name, surname=surname, club=club)
+    health_id = request.args.get('health_id')
+    player = Footballer(name=name, surname=surname, club=club, health_id=health_id)
     db.session.add(player)
     db.session.commit()
     return jsonify(player.json())
+
+@api.route('/healths')
+def get_healths():
+    return jsonify([(lambda health: health.json())(health) for health in Health.query.all()])
+
+@api.route('/health/<int:id>')
+def get_health(id):
+    health = Health.query.get(id)
+    return health.json() if health else 'Health not found'
+
+@api.route('/health/add')
+def put_health():
+    name = request.args.get('name')
+    health = Health(name=name)
+    db.session.add(health)
+    db.session.commit()
+    return jsonify(health.json())
