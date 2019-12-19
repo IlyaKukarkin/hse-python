7fdbab010d087a1aab70ac3a7d9c1b003fac3a2a
iakukarkin <ilya1196@yandex.ru>
2019-12-19 16:43:17 +0500

    add edit routes

--- routes.py
+++ routes.py
@@ -4,6 +4,7 @@ from models import Footballer, db, Club
 index = Blueprint('index', __name__, url_prefix='/')
 api = Blueprint('api', __name__, url_prefix='/api')
 
+
 @index.route('/')
 @index.route('/index')
 def get_index():
@@ -19,6 +20,7 @@ def get_index():
             </html>
     
     '''
+
 
 @api.route('/players')
 def get_players():
@@ -45,14 +47,40 @@ def put_player():
         db.session.commit()
         return jsonify(player.json())
 
+
+@api.route('/player/<int:id>/edit')
+def edit_player(id):
+    player = Footballer.query.get(id)
+    if player:
+        name = request.args.get('name')
+        surname = request.args.get('surname')
+        club_id = request.args.get('club_id')
+        if club_id is not None:
+            club = Club.query.get(club_id)
+            if club is None:
+                return 'Error: club with that ID doesn`t exist'
+            else:
+                player.club_id = club.id
+        if name is not None:
+            player.name = name
+        if surname is not None:
+            player.surname = surname
+        db.session.commit()
+        return jsonify(player.json())
+    else:
+        return 'Player not found'
+
+
 @api.route('/clubs')
 def get_clubs():
     return jsonify([(lambda club: club.json())(club) for club in Club.query.all()])
 
+
 @api.route('/club/<int:id>')
 def get_club(id):
     club = Club.query.get(id)
-    return club.json() if club else 'Health not found'
+    return club.json() if club else 'Club not found'
+
 
 @api.route('/club/add')
 def put_club():
@@ -61,3 +89,16 @@ def put_club():
     db.session.add(club)
     db.session.commit()
     return jsonify(club.json())
+
+
+@api.route('/club/<int:id>/edit')
+def edit_club(id):
+    club = Club.query.get(id)
+    if club:
+        name = request.args.get('name')
+        if name:
+            club.name = name
+        db.session.commit()
+        return jsonify(club.json())
+    else:
+        return 'Club not found'
